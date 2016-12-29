8a53ff865f22f9b09d2db9c2932e73dc67aea1ba
changhuiqiang <chqiangs@gmail.com>
2016-12-29 15:41:00 +0800

    add Welcome page

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,5 +1,5 @@
 angular.module('starter.controllers', [])
-.controller('welcomCtrl',['$scope','Emoji',function(scope,Emoji){
+.controller('welcomCtrl',['$scope','Emoji','$state',function(scope,Emoji,$state){
 	var vm = this;
 	vm.vars = {
 		loademoji:[
@@ -17,11 +17,64 @@ angular.module('starter.controllers', [])
 			"../imgs/animals/770.png",
 			"../imgs/stars/60.png",
 			"../imgs/faces/718.png",
-		]
+		],
+		slidePage:0
 	}
-
+	vm.fn = {
+		init:function(){
+			this.slideScroll();
+		},
+		slideScroll:function(){
+			function timeStart(){
+				vm.vars.timer = setTimeout(function(){
+					if(vm.vars.slidePage<2){
+						vm.vars.slidePage++;
+					}else{
+						vm.vars.slidePage = 0;
+					}
+					scope.$apply();
+					clearTimeout(vm.vars.timer);
+					timeStart();
+				},3000);
+			}
+			timeStart();
+		},
+		swipeLeft:function(){
+			clearTimeout(vm.vars.timer);
+			if(vm.vars.slidePage<2){
+				vm.vars.slidePage++;
+			}else{
+				vm.vars.slidePage = 0;
+			}
+		},
+		swipeRight:function(){
+			clearTimeout(vm.vars.timer);
+			if(vm.vars.slidePage>0){
+				vm.vars.slidePage--;
+			}else{
+				vm.vars.slidePage = 2;
+			}
+		},
+		wechatLogin:function(){
+			//微信登录，这里暂时使用路由跳转
+			$state.go("index");
+		}
+	}
+	vm.fn.init();
 	scope.vm = vm;
 }])
-.controller('indexCtrl',['$scope',function(scope,Emoji){
+.controller('indexCtrl',['$scope','Emoji','$state',function(scope,Emoji,$state){
+	var vm = this;
+	vm.vars = {
+		tooltip:false,
+	}
+	vm.fn = {
+		init:function(){
 
+		}
+	}
+
+	vm.fn.init();
+
+	scope.vm = vm;
 }])
